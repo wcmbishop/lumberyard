7150edbadb0f85c2ef2dd11cb1afa7c8d68e1528
benjoaquin <benjoaquin@lus-bgouverneur2.teslamotors.com>
2016-04-14 07:36:45 -0700

    Adding LumberyardAPI.swift

---
+++ lumberyard/LumberyardAPI.swift
@@ -0,0 +1,39 @@
+//
+//  LumberyardAPI.swift
+//  lumberyard
+//
+//  Created by BenJoaquín Gouverneur on 4/14/16.
+//  Copyright © 2016 Tranquility Base. All rights reserved.
+//
+//  Based on "Chapter 19 Web Services" from iOS Programming The Big Nerd Ranch Guide
+
+import Foundation
+
+enum EndPoint: String {
+    case ToolsList = "/tools/list"
+}
+
+struct LumberyardAPI {
+    private static let baseURLString = "http://yard-dev.teslamotors.com/lumberyard/api/0.1"
+    
+    private static func lumberyardURL(endpoint endpoint: EndPoint,
+                                               parameters: [String:String]?) -> NSURL? {
+        let components = NSURLComponents(string: baseURLString)!
+        
+        var queryItems = [NSURLQueryItem]()
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        return components.URL!
+    }
+    
+    static func toolsListURL() -> NSURL? {
+        return lumberyardURL(endpoint: .ToolsList, parameters: ["tool_class": "ETL"])
+    }
+}
